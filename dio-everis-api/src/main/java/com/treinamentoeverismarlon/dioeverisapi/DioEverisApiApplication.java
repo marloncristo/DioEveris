a4b3d76171fccbff211a1a8a59b9953eb42585e0
marloncristo <marlon.cristo.paula@gmail.com>
2021-06-22 15:53:29 -0300

    Commit inicial

---
+++ dio-everis-api/src/main/java/com/treinamentoeverismarlon/dioeverisapi/DioEverisApiApplication.java
@@ -0,0 +1,13 @@
+package com.treinamentoeverismarlon.dioeverisapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioEverisApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioEverisApiApplication.class, args);
+	}
+
+}
---
+++ dio-everis-api/src/test/java/com/treinamentoeverismarlon/dioeverisapi/DioEverisApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treinamentoeverismarlon.dioeverisapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioEverisApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
